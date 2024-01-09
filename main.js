bdf176b18fcc0542aa18dc3b88d1fab3ca5b57e8
Athul Krishna Madathil Sunil <athulsunil04@gmail.com>
2024-01-09 00:29:51 +0000

    add score keeping table

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const path = 'cards.json';
-const button = document.getElementById('start');
 
 let lastGame = [];
 if (localStorage.getItem('game')) {
@@ -10,75 +9,63 @@ let game = [];
 let selected = [];
 let cardSelected = [];
 let numSelected = 0;
-let time = 0;
+let timer = 0;
+let minutes = 0;
+let seconds = 0;
 let score = 0;
 let tries = 0;
 let set = true;
 
-async function getImages() {
-    const response = await fetch(path);
-    const data = response.json();
-
-    return data;
-}
-
-function firstPage() {
-    const screen = document.getElementById('screen');
-    const cardBox = document.getElementById('card-box');
-    const header = document.createElement('header');
-    const prompt = document.createElement('div');
-    const h2Header = document.createElement('h2');
-    const h2Prompt = document.createElement('h2');
-    const start = document.createElement('button');
-
-    prompt.id = 'prompt';
-    start.id = 'start';
-    h2Header.innerHTML = 'Card Flip!';
-    h2Prompt.innerHTML = 'Start The Game?'
-    start.innerHTML = 'Start!';
-
-    header.append(h2Header);
-    prompt.append(h2Prompt, start);
-    screen.append(header, prompt);
-
-    start.onclick = async () => {
-        let images = [];
-
-        try {
-            images = await getImages();
-        } catch (error) {
-            console.error('Rejected:\n', error);
-        }
-        //shuffleCards(images);
-        makeCards(images);
-    }
-}
-
 function count() {
     set = false;
-    
-    let timer = setInterval(() => {
-        time = Math.round((time + 0.01) * 100) / 100;
+    const mins = document.getElementById('mins');
+    const secs = document.getElementById('secs');
+    const attempts = document.getElementById('tries');
+    const scoreP = document.getElementById('score');
+
+    mins.innerHTML = 0;
+    secs.innerHTML = 0;
+    attempts.innerHTML = 0;
+    scoreP.innerHTML = 0;
+
+    timer = setInterval(() => {
+        seconds++;
+
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+
+        console.log(minutes + ' ' + seconds);
+
+        mins.innerHTML = minutes;
+        secs.innerHTML = seconds;
 
         const flippedCards = document.getElementById('card-box')
                             .querySelectorAll('.remove').length;
         const numOfCards = document.getElementById('card-box')
                             .querySelectorAll('.card').length;
-        console.log(time);
 
         if (flippedCards === numOfCards) {
-            console.log('hi');
-            console.log(time);
+            console.log(minutes + ' ' + seconds);
+            minutes = 0;
+            seconds = 0;
             clearInterval(timer);
         }
-    }, 10);
+    }, 1000);
 }
 
 function cardMatch() {
     setTimeout(() => {
+        const attempts = document.getElementById('tries');
+        const scoreP = document.getElementById('score');
+
         cardSelected.forEach(elem => elem.classList.add('remove'));
         score++;
         tries++;
+
+        attempts.innerHTML = tries;
+        scoreP.innerHTML = score;
         selected = [];
         numSelected = 0;
         cardSelected = [];
@@ -170,6 +157,96 @@ function shuffleCards(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+}
+
+async function getImages() {
+    const response = await fetch(path);
+    const data = response.json();
+
+    return data;
+}
+
+async function generateCards() {
+    let images = [];
+
+        try {
+            images = await getImages();
+        } catch (error) {
+            console.error('Rejected:\n', error);
+        }
+        //shuffleCards(images);
+        makeCards(images);
+}
+
+function secondPage() {
+    const screen = document.getElementById('screen');
+    const cardBox = document.getElementById('card-box');
+
+    const gameScore = document.createElement('div');
+    const scores = document.createElement('div');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
+    const buttons = document.createElement('div');
+    const home = document.createElement('button');
+    const reset = document.createElement('button');
+
+    gameScore.id = 'game-score';
+    scores.id = 'scores';
+    p1.id = 'time';
+    p2.id = 'attempts';
+    p3.id = 'match';
+    buttons.id = 'buttons';
+    home.id = 'home';
+    reset.id = 'reset';
+
+    p1.innerHTML = `Time: <span id="mins">--</span>m : <span id="secs">--</span>s`;
+    p2.innerHTML = `Flips: <span id="tries">--</span>`;
+    p3.innerHTML = `Score: <span id="score">--</span>`;
+
+    screen.innerHTML = '';
+
+    scores.append(p1, p2, p3);
+    buttons.append(home, reset);
+    gameScore.append(scores, buttons);
+    screen.append(gameScore);
+
+    generateCards();    
+}
+
+function firstPage() {
+    const screen = document.getElementById('screen');
+    const cardBox = document.getElementById('card-box');
+    const header = document.createElement('header');
+    const prompt = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const h2Prompt = document.createElement('h2');
+    const start = document.createElement('button');
+
+    prompt.id = 'prompt';
+    start.id = 'start';
+    h1.innerHTML = 'Card Flip!';
+    h2Prompt.innerHTML = 'Start The Game?'
+    start.innerHTML = 'Start!';
+
+    header.append(h1);
+    prompt.append(h2Prompt, start);
+    screen.append(header, prompt);
+
+    start.onclick = () => {
+        secondPage();
+    }
+    /*async () => {
+        let images = [];
+
+        try {
+            images = await getImages();
+        } catch (error) {
+            console.error('Rejected:\n', error);
+        }
+        //shuffleCards(images);
+        makeCards(images);
+    }*/
 }
 
 window.onload = () => {
